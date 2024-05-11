1e05a8fa8c0b8c9025f41a57bd609e569644fa89
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-10 21:47:20 -0300

    Model sucursal

--- app/Models/Sucursal.php
+++ app/Models/Sucursal.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Sucursal extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'direccion',
+        'codigo',
+    ];
+
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+
+    public function productos(): HasMany
+    {
+        return $this->hasMany(Producto::class);
+    }
 }
