3a7843b2d5723062ec86ec4cb15187b802b922c4
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-10 23:49:55 -0300

    rename carpeta de Producto a Product - muestra un grid con los productos

---
+++ resources/js/Pages/Product/Index.jsx
@@ -0,0 +1,36 @@
+import {Head, Link} from "@inertiajs/react";
+import AuthenticatedOrNormalLayout from "@/Layouts/AuthenticatedOrNormalLayout.jsx";
+import Product from "@/Pages/Product/Partials/Product.jsx";
+
+export default function Index({auth, products}) {
+    return (
+        <AuthenticatedOrNormalLayout
+            user={auth.user}
+            authenticatedHeader={
+                <div className="flex gap-3 justify-between items-center">
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                        Productos
+                    </h2>
+                    <Link
+                        className="bg-emerald-500 py-1 px-3 text-white rounded shadow transition-all hover:bg-emerald-600"
+                        href={route('producto.create')}
+                    >
+                        Add new
+                    </Link>
+                </div>
+            }
+            normalHeader={
+                <div className="flex gap-3 justify-between items-center">
+                    <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                        Productos
+                    </h2>
+                </div>
+            }
+        >
+            <Head title="Productos"/>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
+                {products.data.map(product => <Product key={product.id} product={product}></Product>)}
+            </div>
+        </AuthenticatedOrNormalLayout>
+    )
+}
