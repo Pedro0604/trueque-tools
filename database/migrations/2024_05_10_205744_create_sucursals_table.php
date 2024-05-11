9bea2c9382cbee7af4ad7ca05f4118e229ce9e80
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-10 21:09:36 -0300

    files sucursal

--- database/migrations/2024_05_10_205744_create_sucursals_table.php
+++ database/migrations/2024_05_10_205744_create_sucursals_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('sucursals', function (Blueprint $table) {
             $table->id();
+            $table->integer('codigo')->unique();
+            $table->string('nombre');
+            $table->string('direccion');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sucursal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,13 +14,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Sucursal::factory(5)->create();
+
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'test@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
-
-
 
         User::factory(4)->hasProductos(10)->create();
     }
