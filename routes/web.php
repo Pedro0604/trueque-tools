89b1fb0ba11541c43e7f1ba6bd59b14ba58b2c4a
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-12 21:46:43 -0300

    move routes

--- routes/web.php
+++ routes/web.php
@@ -15,17 +15,19 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/producto', [ProductController::class, 'index'])->name('producto.index');
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::resource('producto', ProductController::class)->except(['index']);
+
+    Route::resource('producto', ProductController::class)->except(['index', 'show']);
 });
+
+Route::get('/producto', [ProductController::class, 'index'])->name('producto.index');
+Route::get('/producto/{producto}', [ProductController::class, 'show'])->name('producto.show');
 
 require __DIR__.'/auth.php';
