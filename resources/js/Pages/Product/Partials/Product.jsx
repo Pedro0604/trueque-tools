d242c3a4256632e215ff55f7385c2865cb8e4f08
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-12 21:49:07 -0300

    fix styles | add star for promoted products

--- resources/js/Pages/Product/Partials/Product.jsx
+++ resources/js/Pages/Product/Partials/Product.jsx
@@ -1,3 +1,6 @@
+import {CATEGORIES_TEXT_MAP} from "@/Categories.jsx";
+import StarIcon from '@mui/icons-material/Star';
+
 export default function Product({product}) {
     const showProduct = (productId) => {
         // TODO - Descomentar cuando se cree la ruta product.show
@@ -9,22 +12,24 @@ export default function Product({product}) {
     return (
         <div
             onClick={() => showProduct(product.id)}
-            className="bg-gray-200 dark:bg-gray-700 lg:bg-gray-100 lg:dark:bg-gray-800
-            lg:hover:bg-gray-200 lg:hover:dark:bg-gray-700 lg:hover:shadow-2xl transition-all
-            rounded-lg p-4 cursor-pointer"
+            className={`bg-gray-200 dark:bg-gray-700 lg:bg-gray-100 lg:dark:bg-gray-800
+                lg:hover:bg-gray-200 lg:hover:dark:bg-gray-700 lg:hover:shadow-2xl transition-all
+                rounded-lg p-4 cursor-pointer`}
         >
-            <p className="text-gray-600 dark:text-gray-400 text-sm">{product.user.name}</p>
+            <div className="flex justify-between items-center mb-1">
+                <p className="text-gray-600 dark:text-gray-400 text-sm">{product.user.name}</p>
+                {product.promoted_at && <StarIcon className="text-yellow-500"/>}
+            </div>
             <img
                 src={product.image_path}
                 alt={product.name}
-                className="w-full object-cover h-64 rounded-md"
+                className={`w-full object-cover h-64 rounded-md`}
             />
             <div className="mt-4">
-                <h2 className="text-xl font-semibold">{product.name}</h2>
-                <p className="text-gray-600 dark:text-gray-400">{product.description}</p>
-                <p className="text-gray-600 dark:text-gray-400">Categoria: {product.category}</p>
+                <p className="text-gray-600 dark:text-gray-400 text-ellipsis">{product.name}</p>
+                <p className="text-xl">{CATEGORIES_TEXT_MAP[product.category]}</p>
+                <p className="text-gray-600 dark:text-gray-400 overflow-hidden text-ellipsis">{product.description}</p>
                 <p className="text-gray-600 dark:text-gray-400">Sucursal: {product.sucursal.name}</p>
-                {product.promoted_at && <p className="text-gray-600 dark:text-gray-400">Promocionado</p>}
             </div>
         </div>
     )
