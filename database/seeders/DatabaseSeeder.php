48be2fbd4935538d2a08a663476dc9d29687bdad
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-19 20:29:31 -0300

    seeder para demo1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Sucursal::factory(5)->create();
+//        Sucursal::factory(5)->create();
+
+        Sucursal::factory()->create([
+            'name' => 'Sucursal 1',
+            'address' => 'Calle 1 n°1234',
+            'code' => '12345',
+        ]);
+        Sucursal::factory()->create([
+            'name' => 'Sucursal 2',
+            'address' => 'Calle 2 n°2345',
+            'code' => '23456',
+        ]);
+        Sucursal::factory()->create([
+            'name' => 'Sucursal 3',
+            'address' => 'Calle 3 n°3456',
+            'code' => '34567',
+        ]);
+        Sucursal::factory()->create([
+            'name' => 'Sucursal 4',
+            'address' => 'Calle 4 n°4567',
+            'code' => '45678',
+        ]);
+        Sucursal::factory()->create([
+            'name' => 'Sucursal 5',
+            'address' => 'Calle 5 n°5678',
+            'code' => '56789',
+        ]);
 
         User::factory()->create([
             'name' => 'Test User',
@@ -22,6 +48,6 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('12341234'),
         ]);
 
-        User::factory(4)->hasProducts(10)->create();
+//        User::factory(4)->hasProducts(10)->create();
     }
 }
