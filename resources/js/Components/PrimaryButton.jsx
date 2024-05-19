3b7e5c81fdd762bc5591324a57c5fb090ce07c54
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-18 22:27:40 -0300

    agrega posibilidad de ser link, modifica colores para que sea del beige dado

--- resources/js/Components/PrimaryButton.jsx
+++ resources/js/Components/PrimaryButton.jsx
@@ -1,15 +1,30 @@
-export default function PrimaryButton({ className = '', disabled, children, ...props }) {
+import {Link} from "@inertiajs/react";
+
+export default function PrimaryButton({className = '', isLink, children, ...props}) {
+    const classes = `inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-custom-beige-300
+                border border-transparent rounded-md font-semibold text-xs text-white
+                dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700
+                dark:hover:bg-custom-beige-100 focus:bg-gray-700 dark:focus:bg-custom-beige-500
+                active:bg-gray-900 dark:active:bg-custom-beige-500 focus:outline-none focus:ring-2
+                focus:ring-custom-teal-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800
+                transition ease-in-out duration-150 disabled:opacity-25 disabled:hover:bg-custom-beige-300 ${className}`;
+
+    if (isLink) {
+        return (
+            <Link
+                {...props}
+                className={classes}
+            >
+                {children}
+            </Link>
+        )
+    }
     return (
         <button
             {...props}
-            className={
-                `inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150 ${
-                    disabled && 'opacity-25'
-                } ` + className
-            }
-            disabled={disabled}
+            className={classes}
         >
             {children}
         </button>
-    );
+    )
 }
