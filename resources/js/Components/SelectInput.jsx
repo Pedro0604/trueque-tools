ddfe090dbac0e5707430407909fec893934d0b31
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-18 22:37:51 -0300

    cambia el color del borde a custom-teal

--- resources/js/Components/SelectInput.jsx
+++ resources/js/Components/SelectInput.jsx
@@ -1,16 +1,18 @@
 import {forwardRef, useRef} from 'react';
 
 export default forwardRef(function SelectInput({className = '', children, ...props}, ref) {
-  const select = ref ? ref : useRef();
+    const select = ref ? ref : useRef();
 
-  return (
-    <select
-      {...props}
-      className={
-        'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
-        className
-      }
-      ref={select}
-    >{children}</select>
-  );
+    return (
+        <select
+            {...props}
+            className={
+                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 ' +
+                ' focus:border-custom-teal-500 dark:focus:border-custom-teal-600 focus:ring-custom-teal-500 ' +
+                ' dark:focus:ring-custom-teal-600 rounded-md shadow-sm ' +
+                className
+            }
+            ref={select}
+        >{children}</select>
+    );
 });
--- resources/js/Components/TextAreaInput.jsx
+++ resources/js/Components/TextAreaInput.jsx
@@ -1,22 +1,24 @@
 import {forwardRef, useEffect, useRef} from 'react';
 
 export default forwardRef(function TextInput({className = '', isFocused = false, children, ...props}, ref) {
-  const textArea = ref ? ref : useRef();
+    const textArea = ref ? ref : useRef();
 
-  useEffect(() => {
-    if (isFocused) {
-      textArea.current.focus();
-    }
-  }, []);
+    useEffect(() => {
+        if (isFocused) {
+            textArea.current.focus();
+        }
+    }, []);
 
-  return (
-    <textarea
-      {...props}
-      className={
-        'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm ' +
-        className
-      }
-      ref={textArea}
-    >{children}</textarea>
-  );
+    return (
+        <textarea
+            {...props}
+            className={
+                'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 ' +
+                ' focus:border-custom-teal-500 dark:focus:border-custom-teal-600 focus:ring-custom-teal-500 ' +
+                ' dark:focus:ring-custom-teal-600 rounded-md shadow-sm ' +
+                className
+            }
+            ref={textArea}
+        >{children}</textarea>
+    );
 });
