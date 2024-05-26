b75ca0130be8db40a17d7c29bb1935bbc96ad101
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-26 15:18:04 -0300

    Espaciado entre comentarios

--- resources/js/Pages/Comment/CommentsList.jsx
+++ resources/js/Pages/Comment/CommentsList.jsx
@@ -1,6 +1,7 @@
 import Comment from "./Partials/Comment";
 
 export default function CommentsList({ comments }) {
+    console.log(JSON.stringify(comments, null, 2))
     return (
         <div className="p-6 bg-gray-300 dark:bg-gray-700 rounded-b-lg rounded-t-sm">
             {comments ? (
@@ -8,19 +9,22 @@ export default function CommentsList({ comments }) {
                     <h2 className="text-2xl font-bold text-black dark:text-white mb-2">
                         Comentarios:
                     </h2>
-                    {comments.map((comment) => (
-                        <div className="mb-3">
-                            <Comment key={comment.id} comment={comment} />
-                            {comment.response && (
-                                <Comment
-                                    key={comment.response.id}
-                                    comment={comment.response}
-                                    isResponse
-                                    className={`mt-2`}
-                                />
-                            )}
-                        </div>
-                    ))}
+                    <div className="flex flex-col gap-4">
+                        {comments.map((comment) => (
+                            <div
+                                key={comment.id}
+                            >
+                                <Comment comment={comment} />
+                                {comment.response && (
+                                    <Comment
+                                        comment={comment.response}
+                                        isResponse
+                                        className={`mt-2`}
+                                    />
+                                )}
+                            </div>
+                        ))}
+                    </div>
                 </>
             ) : (
                 <h2>No hay comentarios</h2>
