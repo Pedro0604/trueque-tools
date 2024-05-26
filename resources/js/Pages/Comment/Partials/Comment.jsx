83fa7b1896d31ad2a34ad7e57adcf010a8893a89
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-26 15:17:55 -0300

    Flecha entre comentario y su respuesta

--- resources/js/Pages/Comment/Partials/Comment.jsx
+++ resources/js/Pages/Comment/Partials/Comment.jsx
@@ -9,8 +9,9 @@ export default function Comment({
             {...props}
             className={`${
                 isResponse ? "ml-10 " : ""
-            } text-black dark:text-white bg-gray-400 dark:bg-gray-800 rounded-md p-4 ${className}`}
+            } text-black dark:text-white bg-gray-400 dark:bg-gray-800 rounded-md p-4 relative ${className}`}
         >
+            {isResponse && <div className="absolute border-b border-l border-gray-500 dark:border-gray-300 w-4 h-4 -left-6 top-1"></div>}
             <div className="flex items-center justify-between">
                 <div className="font-semibold text-sm text-gray-600 dark:text-gray-500">
                     {comment.user.name}
