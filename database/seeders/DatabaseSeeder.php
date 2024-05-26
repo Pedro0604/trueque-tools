7156f41b8ab8eb4ff640dc4c03e80d0d2bf99ce2
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-25 22:51:05 -0300

    Añade llamada a ProductSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             SucursalSeeder::class,
             UserSeeder::class,
+            ProductSeeder::class,
             CommentSeeder::class,
         ]);
     }
