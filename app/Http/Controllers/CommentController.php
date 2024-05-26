bc1b587f210091750ee0cdabbd7d8a8dca881ac0
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-25 22:39:52 -0300

    elimina index y create porque se van a mostrar desde Product.Show, no van a tener su propia ruta - añade store y update requests

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,43 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\commentResource;
-use App\Http\Resources\ProductResource;
+use App\Http\Requests\StoreCommentRequest;
+use App\Http\Requests\UpdateCommentRequest;
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
-use App\Models\Product;
-use http\Env\Response;
 use Illuminate\Http\Request;
-use Inertia\Inertia;
+use Inertia\Response;
 use Inertia\ResponseFactory;
 
 class CommentController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     */
-    public function index(): Response|ResponseFactory
-    {
-        $comments = Comment::all()->sortByDesc('created_at');
-        return Inertia::render('Comment/index', [
-            'comments' => commentResource::collection($comments),
-            'commentCreatedId' => session('comment_created_id'),
-        ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create(): Response|ResponseFactory
-    {
-        return inertia('Comment/Create', [
-            'products' => ProductResource::collection(Product::all()),      // O aca seria linkear al producto sobre el que quiero commentar?
-        ]);
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCommentRequest $request)
     {
         //
     }
@@ -62,7 +39,7 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Comment $comments)
+    public function update(UpdateCommentRequest $request, Comment $comments)
     {
         //
     }
