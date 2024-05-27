040a0a0f4ccfb1ef563b5a5285250dbefa6210eb
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-26 22:05:17 -0300

    Añade relaciones con el producto publicado y el producto ofertado

--- app/Models/Solicitud.php
+++ app/Models/Solicitud.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Solicitud extends Model
 {
@@ -14,4 +15,14 @@ class Solicitud extends Model
         'offered_product_id',
         'was_rejected',
     ];
+
+    public function publishedProduct(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'published_product_id');
+    }
+
+    public function offeredProduct(): BelongsTo
+    {
+        return $this->belongsTo(Product::class, 'offered_product_id');
+    }
 }
