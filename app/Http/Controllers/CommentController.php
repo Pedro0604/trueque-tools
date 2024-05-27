92fb58cc4f405df3e7b0316ad49aad4553990719
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-26 21:02:00 -0300

    Validacion

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreCommentRequest;
+use App\Http\Requests\StoreResponseRequest;
 use App\Http\Requests\UpdateCommentRequest;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
@@ -33,6 +34,19 @@ class CommentController extends Controller
      */
     public function respond(StoreCommentRequest $request, Comment $comment): RedirectResponse
     {
+        if($comment->response_id !== null) {
+            return back()->with('error', 'Este comentario ya tiene una respuesta');
+        }
+        if($comment->product_id === null) {
+            return back()->with('error', 'No se puede responder una respuesta');
+        }
+        if($comment->user_id === auth()->id()) {
+            return back()->with('error', 'No podés responder tu propio comentario');
+        }
+        if($comment->product->user->id !== auth()->id()) {
+            return back()->with('error', 'Solo podés responder a comentarios en tus productos');
+        }
+
         $data = $request->validated();
         $data['user_id'] = auth()->id();
         $data['product_id'] = null;
