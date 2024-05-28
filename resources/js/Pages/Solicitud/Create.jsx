b743fe80d0e8db0a3595414ae94408a461f7670c
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-28 17:35:12 -0300

    Arregla ruta de post - Añade label y errores para meeting_date_time

--- resources/js/Pages/Solicitud/Create.jsx
+++ resources/js/Pages/Solicitud/Create.jsx
@@ -9,6 +9,9 @@ import Modal from "@mui/material/Modal";
 import {useState} from "react";
 import {Box} from "@mui/material";
 import ShowAvailableProducts from "@/Pages/Solicitud/Partials/ShowAvailableProducts.jsx";
+import InputError from "@/Components/Inputs/InputError.jsx";
+import InputLabel from "@/Components/Inputs/InputLabel.jsx";
+import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 
 const style = {
     position: 'absolute',
@@ -19,7 +22,7 @@ const style = {
     p: 4,
 };
 
-export default function create({publishedProduct, availableProducts}) {
+export default function create({publishedProduct, availableProducts, sucursals}) {
     const {auth} = usePage().props;
 
     const [open, setOpen] = useState(false);
@@ -41,7 +44,7 @@ export default function create({publishedProduct, availableProducts}) {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        post(route('solicitud.create', publishedProduct.id))
+        post(route('solicitud.store', publishedProduct.id))
     }
 
     const handleSelectProduct = (product) => {
@@ -75,34 +78,62 @@ export default function create({publishedProduct, availableProducts}) {
                     </div>
                     <div className="flex flex-col justify-center items-center gap-4">
                         <div
-                            className="flex items-center gap-1 mt-1"
+                            className="flex items-center gap-1 mt-1 text-gray-600 dark:text-custom-beige-300"
                         >
                             <BusinessIcon
-                                sx={{fontSize: 32}}/>
-                            <p className="text-sm sm:text-base lg:text-xl text-gray-600 dark:text-custom-beige-600">
+                                sx={{fontSize: 32}}
+                            />
+                            <p className="text-sm sm:text-base lg:text-xl">
                                 {publishedProduct.sucursal.name}
                             </p>
                         </div>
-                        <MultipleStopIcon
-                            sx={{fontSize: 40}}
-                        />
+                        <div className="">
+                            <MultipleStopIcon
+                                sx={{fontSize: 40}}
+                            />
+                        </div>
                         <div>
                             <form
                                 onSubmit={handleSubmit}
                             >
-                                <TextInput
-                                    id={'meeting_date_time'}
-                                    type={'datetime-local'}
+                                <InputLabel
+                                    htmlFor="meeting_date_time"
+                                    value="Fecha y hora del trueque"
+                                    className="!text-lg text-center mb-1"
                                 />
+
+                                <TextInput
+                                    id='meeting_date_time'
+                                    type='datetime-local'
+                                    name="meeting_date_time"
+                                    value={data.meeting_date_time}
+                                    onChange={e => {
+                                        setData('meeting_date_time', e.target.value)
+                                    }}
+                                    invalid={errors.meeting_date_time}
+                                />
+
+                                <InputError message={errors.meeting_date_time} className="mt-2"/>
+
+                                <CyanButton
+                                    className="w-full justify-center mt-10"
+                                    disabled={processing}
+                                >
+                                    Solicitar trueque
+                                </CyanButton>
                             </form>
                         </div>
                     </div>
                     {selectedProduct === null ?
                         <div
                             className={`bg-gray-200 dark:bg-gray-700 lg:bg-gray-100 lg:dark:bg-gray-800
-                                        lg:hover:bg-gray-200 lg:hover:dark:bg-custom-gray-700 lg:hover:shadow-2xl transition-all
-                                        rounded-lg p-4 cursor-pointer border border-custom-beige-900 dark:border-custom-beige-500
-                                        flex justify-center items-center w-72`}
+                                lg:hover:bg-gray-200 lg:hover:dark:bg-custom-gray-700 lg:hover:shadow-2xl transition-all
+                                rounded-lg p-4 cursor-pointer border border-custom-beige-900 dark:border-custom-beige-500
+                                flex justify-center items-center w-72 ${errors.offered_product_id ? 'border-red-600' +
+                                ' dark:border-red-400 focus:border-orange-900 dark:focus:border-orange-800 focus:ring-orange-900' +
+                                ' dark:focus:ring-orange-800' : ''}`}
+                            // TODO - ver como funciona el tabIndex
+                            tabIndex={3}
                             onClick={handleOpen}
                         >
                             <AddIcon
@@ -110,11 +141,15 @@ export default function create({publishedProduct, availableProducts}) {
                             />
                         </div>
                         :
-                        <div className="w-72">
+                        <div
+                            className="w-72"
+                        >
                             <Product
                                 product={selectedProduct}
-                                withSucursal={true}
+                                withSucursal={false}
                                 onClick={handleOpen}
+                                created={true}
+                                key={selectedProduct.id}
                             />
                         </div>
                     }
@@ -133,6 +168,7 @@ export default function create({publishedProduct, availableProducts}) {
                     <ShowAvailableProducts
                         availableProducts={availableProducts}
                         onSelectProduct={handleSelectProduct}
+                        sucursals={sucursals}
                     />
                 </Box>
             </Modal>
