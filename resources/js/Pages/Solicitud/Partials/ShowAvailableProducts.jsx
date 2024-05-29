4eae09dfd4b3ac94ee475bedf8669b0284dd403a
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-28 22:29:12 -0300

    Añade redireccion, onVolver y onSuccess props a Product/Partials/CreateForm

--- resources/js/Pages/Solicitud/Partials/ShowAvailableProducts.jsx
+++ resources/js/Pages/Solicitud/Partials/ShowAvailableProducts.jsx
@@ -1,25 +1,35 @@
 import Product from "@/Pages/Product/Partials/Product.jsx";
-import {useState} from "react";
+import { useState } from "react";
 import CreateForm from "@/Pages/Product/Partials/CreateForm";
 
-export default function ShowAvailableProducts({availableProducts, onSelectProduct, sucursals, publishedProduct}) {
-    const [showCreateProductForm, setShowCreateForm] = useState(false)
+export default function ShowAvailableProducts({
+    availableProducts,
+    onSelectProduct,
+    sucursals,
+    publishedProduct,
+    onCloseModal,
+}) {
+    const [showCreateProductForm, setShowCreateForm] = useState(false);
 
-    // TODO - HACER FORMULARIO SCROLLABLE
-    if(showCreateProductForm){
-        return(
+    if (showCreateProductForm) {
+        return (
             <CreateForm
                 sucursals={sucursals}
                 selectedSucursal={publishedProduct.sucursal}
                 selectedCategory={publishedProduct.category}
+                onVolver={() => setShowCreateForm(false)}
+                redirectionOnSuccess={route(
+                    "solicitud.create",
+                    publishedProduct.id
+                )}
+                onSuccess={() => onCloseModal}
             />
-        )
+        );
     }
     return (
-        // TODO - AÑADIR SLIDER PARA MOSTRAR MAS DE 3 PRODUCTOS
-        <div
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {availableProducts.map(product => (
+        // TODO - PONER QUE EL AGREGAR PRODUCTO SEA DEL TAMAÑO CORRECTO SI ESTA EN UNA NUEVA FILA
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {availableProducts.map((product) => (
                 <Product
                     key={product.id}
                     product={product}
@@ -27,10 +37,11 @@ export default function ShowAvailableProducts({availableProducts, onSelectProduc
                     withUserName={false}
                     withSucursal={false}
                     withCategory={false}
-                />))}
+                />
+            ))}
             <div
                 onClick={() => setShowCreateForm(true)}
-                className={`bg-gray-200 dark:bg-gray-700 lg:bg-gray-100 lg:dark:bg-gray-800
+                className={`bg-gray-200 dark:bg-gray-700 text-black dark:text-white lg:bg-gray-100 lg:dark:bg-gray-800
                 lg:hover:bg-gray-200 lg:hover:dark:bg-custom-gray-700 lg:hover:shadow-2xl transition-all
                 rounded-lg p-4 cursor-pointer border border-custom-beige-900 dark:border-custom-beige-500
                 flex justify-center items-center`}
@@ -38,5 +49,5 @@ export default function ShowAvailableProducts({availableProducts, onSelectProduc
                 Crear un nuevo producto
             </div>
         </div>
-    )
+    );
 }
