58530ff0f5b0a5fe4b8c2790b80c760a55689d47
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-29 19:48:21 -0300

    Agrega validacion para chequear que el producto no haya sido trocado

--- app/Http/Resources/SolicitudResource.php
+++ app/Http/Resources/SolicitudResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Product;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -17,11 +18,37 @@ class SolicitudResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'published_product_id' => [
+                'required',
+                'integer',
+                'exists:products,id',
+                function ($attribute, $value, $fail) {
+                    $product = Product::find($value);
+                    if ($product->publishedTrueque || $product->offeredTrueque) {
+                        $fail('El producto publicado ya fue trocado.');
+                    }
+                },
+            ],
+            'offered_product_id' => [
+                'required',
+                'integer',
+                'exists:products,id',
+                'different:published_product_id',
+                function ($attribute, $value, $fail) {
+                    $product = Product::find($value);
+                    if ($product->publishedTrueque || $product->offeredTrueque) {
+                        $fail('El producto ofertado ya fue trocado.');
+                    }
+                    if ($product->offeredSolicituds()->where('published_product_id', $this->published_product_id)->exists()) {
+                        $fail('El producto ofertado ya tiene una solicitud para el producto publicado.');
+                    }
+                },
+            ],
             'published_product' => new ProductResource($this->publishedProduct),
             'offered_product' => new ProductResource($this->offeredProduct),
-            'meeting_date_time' =>  (new Carbon($this->meeting_date_time))->format('d/m/Y H:i'),
+            'meeting_date_time' => (new Carbon($this->meeting_date_time))->format('d/m/Y H:i'),
             'was_rejected' => $this->was_rejected,
-            'created_at' =>  (new Carbon($this->created_at))->format('d/m/Y H:i'),
+            'created_at' => (new Carbon($this->created_at))->format('d/m/Y H:i'),
         ];
     }
 }
