f6389734d8bb6cdf77676928e340d6bc0407f1f7
Pedro0604 <pedrospadari0604@gmail.com>
2024-05-31 20:04:15 -0300

    Mueve ProductsList a Product/Partials

--- resources/js/Pages/Product/Index.jsx
+++ resources/js/Pages/Product/Index.jsx
@@ -1,9 +1,8 @@
 import {Head} from "@inertiajs/react";
 import AuthenticatedOrNormalLayout from "@/Layouts/AuthenticatedOrNormalLayout.jsx";
-import Product from "@/Pages/Product/Partials/Product.jsx";
 import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 import AddIcon from '@mui/icons-material/Add';
-import ProductsList from "@/Pages/Product/ProductsList.jsx";
+import ProductsList from "@/Pages/Product/Partials/ProductsList.jsx";
 
 export default function Index({auth, products, productCreatedId = null}) {
     return (
@@ -29,22 +28,6 @@ export default function Index({auth, products, productCreatedId = null}) {
             <Head title="Productos"/>
 
             <ProductsList products={products} productCreatedId={productCreatedId}/>
-
-            {/* <div className="text-black dark:text-white bg-gray-100 dark:bg-gray-800 p-4 sm:p-6 md:p-8 rounded-lg">
-                {products.length > 0 ?
-                    <div
-                        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
-                        {products.map(product => (
-                            <Product
-                                key={product.id}
-                                created={productCreatedId === product.id}
-                                product={product}
-                            />))}
-                    </div>
-                    :
-                    <h3 className="text-center text-3xl font-bold">No hay productos cargados en el sistema</h3>
-                }
-            </div> */}
         </AuthenticatedOrNormalLayout>
     )
 }
--- resources/js/Pages/Product/MyProducts.jsx
+++ resources/js/Pages/Product/MyProducts.jsx
@@ -2,7 +2,7 @@ import AuthenticatedOrNormalLayout from "@/Layouts/AuthenticatedOrNormalLayout.j
 import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 import AddIcon from "@mui/icons-material/Add";
 import {Head} from "@inertiajs/react";
-import ProductsList from "@/Pages/Product/ProductsList.jsx";
+import ProductsList from "@/Pages/Product/Partials/ProductsList.jsx";
 
 export default function MyProducts({auth, products, productCreatedId = null}) {
     return (
@@ -27,23 +27,9 @@ export default function MyProducts({auth, products, productCreatedId = null}) {
         >
             <Head title="Mis Productos"/>
 
-            <ProductsList products={products} productCreatedId={productCreatedId}/>
-
-            {/* <div className="text-black dark:text-white bg-gray-100 dark:bg-gray-800 p-4 sm:p-6 md:p-8 rounded-lg">
-                {products.length > 0 ?
-                    <div
-                        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-8">
-                        {products.map(product => (
-                            <Product
-                                key={product.id}
-                                created={productCreatedId === product.id}
-                                product={product}
-                            />))}
-                    </div>
-                    :
-                    <h3 className="text-center text-3xl font-bold">No hay productos cargados en el sistema</h3>
-                }
-            </div> */}
+            <ProductsList
+                products={products}
+                productCreatedId={productCreatedId}/>
         </AuthenticatedOrNormalLayout>
     )
 }
