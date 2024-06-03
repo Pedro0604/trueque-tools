ca8d9677b16ce854c881b4a993039a26eade5858
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-02 22:28:23 -0300

    Añade indice unicos para evitar que haya dos trueques con la misma solicitud

--- database/migrations/2024_05_27_122815_create_trueques_table.php
+++ database/migrations/2024_05_27_122815_create_trueques_table.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->dateTime('ended_at')->nullable();
             $table->boolean('is_failed')->default(false);
-            $table->foreignId('solicitud_id')->constrained();
+            $table->foreignId('solicitud_id')->unique()->constrained();
             $table->softDeletes();
             $table->timestamps();
         });
