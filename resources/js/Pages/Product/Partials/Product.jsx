fc67bb737ea3829579a4380ac6f01e047b49ecd8
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-03 12:35:16 -0300

    Añade loading state a las imagenes

--- resources/js/Pages/Product/Partials/Product.jsx
+++ resources/js/Pages/Product/Partials/Product.jsx
@@ -4,8 +4,24 @@ import {router} from "@inertiajs/react";
 import {useEffect, useState} from "react";
 import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
 
-export default function Product({product, created, onClick = null, withUserName = true, withSucursal = true, withCategory = true, className = "", ...props}) {
+export default function Product({
+                                    product,
+                                    created,
+                                    onClick = null,
+                                    withUserName = true,
+                                    withSucursal = true,
+                                    withCategory = true,
+                                    minImageWidth = "min-w-24",
+                                    className = "",
+                                    ...props
+                                }) {
     const [isPulsing, setIsPulsing] = useState(created);
+    const [isLoading, setIsLoading] = useState(true);
+
+    const handleImageLoad = () => {
+        setIsLoading(false);
+    }
+
     const showProduct = (productId) => {
         router.get(route('product.show', productId));
     }
@@ -24,25 +40,41 @@ export default function Product({product, created, onClick = null, withUserName 
 
         <div
             {...props}
-            onClick={onClick ? onClick : ()=> showProduct(product.id)}
+            onClick={onClick ? onClick : () => showProduct(product.id)}
             className={`bg-gray-200 dark:bg-gray-700 lg:bg-gray-100 lg:dark:bg-gray-800
                 lg:hover:bg-gray-200 lg:hover:dark:bg-custom-gray-700 lg:hover:shadow-2xl transition-all
                 rounded-lg p-4 cursor-pointer border border-custom-beige-900 dark:border-custom-beige-500
                 ${isPulsing ? 'animate-pulse' : ''} ${className}`}
         >
             <div className="flex justify-between items-center mb-1">
-                {withUserName && <p className="text-gray-600 dark:text-custom-beige-600 text-sm">{product.user.name}</p>}
+                {withUserName &&
+                    <p className="text-gray-600 dark:text-custom-beige-600 text-sm">{product.user.name}</p>}
                 {/*TODO - DESCOMENTAR CUANDO ESTE HABILITADA LA FUNCION DE PROMOCIONAR */}
                 {/*{product.promoted_at && <StarIcon className="text-yellow-500"/>}*/}
             </div>
             <div className="flex gap-2 sm:gap-4 sm:flex-col overflow-hidden">
-                <div className="w-2/5 min-w-24 sm:w-full">
+                <div className={`w - 2 / 5 ${minImageWidth} sm:w-full`}>
                     {product.image_path ?
-                        <img
-                            src={product.image_path}
-                            alt={product.name}
-                            className={`object-cover w-full aspect-square rounded-md  border border-custom-beige-900 dark:border-custom-beige-500`}
-                        />
+                        <>
+                            {isLoading &&
+                                <div
+                                    className="flex items-center justify-center w-full aspect-square rounded-md bg-gray-200 text-gray-600 dark:bg-gray-600 dark:text-gray-200">
+                                    <p className="hidden sm:inline">Cargando imagen...</p>
+                                    <span className="sm:hidden">
+                                        <ImageNotSupportedIcon
+                                            sx={{fontSize: 32}}
+                                        />
+                                    </span>
+                                </div>
+                            }
+                            <img
+                                src={product.image_path}
+                                alt={product.name}
+                                onLoad={handleImageLoad}
+                                style={{display: isLoading ? 'none' : 'block'}}
+                                className={`object-cover w-full aspect-square rounded-md  border border-custom-beige-900 dark:border-custom-beige-500`}
+                            />
+                        </>
                         :
                         <div
                             className="flex items-center justify-center w-full aspect-square rounded-md bg-gray-200 text-gray-600 dark:bg-gray-600 dark:text-gray-200">
