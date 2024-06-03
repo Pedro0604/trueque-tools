2c3e1e83f277a0f761f49aa439f39a222efcfb00
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-02 22:33:04 -0300

    Evita que haya solicitudes que no fueron rechazadas con los mismos productos

--- database/factories/SolicitudFactory.php
+++ database/factories/SolicitudFactory.php
@@ -17,11 +17,26 @@ class SolicitudFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            'published_product_id' => fake()->numberBetween(1, Product::count()),
-            'offered_product_id' => fake()->numberBetween(1, Product::count()),
-            'meeting_date_time' => fake()->dateTimeBetween('now', '+1 year'), // '2023-05-26 21:07:01
-            'was_rejected' => false,
-        ];
+        $was_rejected = $this->faker->boolean(30); // 30% chance that the solicitud is rejected
+
+        if ($was_rejected) {
+            return [
+                'published_product_id' => $this->faker->numberBetween(1, Product::count()),
+                'offered_product_id' => $this->faker->numberBetween(1, Product::count()),
+                'meeting_date_time' => $this->faker->dateTimeBetween('now', '+1 year'), // '2023-05-26 21:07:01
+                'was_rejected' => $was_rejected,
+            ];
+        } else {
+            // Get unique combinations of published_product_id and offered_product_id
+            $published_product_id = Product::all()->random()->id;
+            $offered_product_id = Product::where('id', '!=', $published_product_id)->get()->random()->id;
+
+            return [
+                'published_product_id' => $published_product_id,
+                'offered_product_id' => $offered_product_id,
+                'meeting_date_time' => $this->faker->dateTimeBetween('now', '+1 year'), // '2023-05-26 21:07:01
+                'was_rejected' => $was_rejected,
+            ];
+        }
     }
 }
