4c62b0c6d41106e00a36d8dc52f422cd8f9ad55c
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-03 12:42:00 -0300

    Añade soft deletes

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,10 +9,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOneThrough;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'name',
--- app/Models/Trueque.php
+++ app/Models/Trueque.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Trueque extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'ended_at',
