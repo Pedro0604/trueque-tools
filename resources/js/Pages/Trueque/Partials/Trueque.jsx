47708087ff02a6363d95da8ed7b56d40ff271f8e
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-02 23:14:23 -0300

    Muestra fecha y hora de trueque y acomoda estilos

--- resources/js/Pages/Trueque/Partials/Trueque.jsx
+++ resources/js/Pages/Trueque/Partials/Trueque.jsx
@@ -1,10 +1,6 @@
 import Product from "@/Pages/Product/Partials/Product.jsx";
 import BusinessIcon from "@mui/icons-material/Business.js";
 import MultipleStopIcon from "@mui/icons-material/MultipleStop.js";
-import InputLabel from "@/Components/Inputs/InputLabel.jsx";
-import TextInput from "@/Components/Inputs/TextInput.jsx";
-import InputError from "@/Components/Inputs/InputError.jsx";
-import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 
 export default function Trueque({trueque, onClick = null, className = "", ...props}) {
 
@@ -21,36 +17,46 @@ export default function Trueque({trueque, onClick = null, className = "", ...pro
             className={`bg-gray-200 dark:bg-gray-700 lg:bg-gray-100 lg:dark:bg-gray-800
                 lg:hover:bg-gray-200 lg:hover:dark:bg-custom-gray-700 lg:hover:shadow-2xl transition-all
                 rounded-lg p-4 cursor-pointer border border-custom-beige-900 dark:border-custom-beige-500
-                flex gap-4 ${className}`}
+                 ${className}`}
         >
-            <Product
-                product={trueque.solicitud.published_product}
-                withCategory={false}
-                withSucursal={false}
-            />
-            <div className="flex flex-col justify-center items-center gap-4">
-                <div className="flex items-center gap-1 mt-1 text-gray-600 dark:text-custom-beige-300">
-                    <BusinessIcon sx={{fontSize: 32}}/>
-                    <p className="text-sm sm:text-base lg:text-xl">
-                        {trueque.solicitud.published_product.sucursal.name}
-                    </p>
-                </div>
-                <div className="">
-                    <MultipleStopIcon sx={{fontSize: 40}}/>
-                </div>
-                <div className="flex items-center gap-1 mt-1 text-gray-600 dark:text-custom-beige-300">
-                    <BusinessIcon sx={{fontSize: 32}}/>
-                    <p className="text-sm sm:text-base lg:text-xl">
-                            CATEGORIA PONER SIMBOLO PESOS Y NOMBRE
-                        {trueque.solicitud.published_product.category}
-                    </p>
-                </div>
+            <h2 className="text-2xl text-center">
+                {trueque.ended_at ?
+                    ("Trocado el: " + trueque.ended_at) :
+                    ("Fecha del trueque: " + trueque.solicitud.meeting_date_time)
+                }
+            </h2>
+            <div className="flex justify-center items-center gap-1 mt-1 text-gray-600 dark:text-custom-beige-300">
+                <BusinessIcon/>
+                <p className="text-sm sm:text-base lg:text-lg">
+                    {trueque.solicitud.published_product.sucursal.name}
+                </p>
             </div>
-            <Product
-                withCategory={false}
-                withSucursal={false}
-                product={trueque.solicitud.offered_product}
-            />
+            <div className="flex justify-center items-center gap-1 mt-1 text-gray-600 dark:text-custom-beige-300">
+                <BusinessIcon/>
+                <p className="text-sm sm:text-base lg:text-lg">
+                    CATEGORIA:
+                    {trueque.solicitud.published_product.category}
+                </p>
+            </div>
+            <div className="flex gap-4">
+                <Product
+                    product={trueque.solicitud.published_product}
+                    withCategory={false}
+                    withSucursal={false}
+                    onClick={() => {
+                    }}
+                />
+                <div className="flex flex-col justify-center items-center gap-4">
+                    <div className="">
+                        <MultipleStopIcon sx={{fontSize: 40}}/>
+                    </div>
+                </div>
+                <Product
+                    withCategory={false}
+                    withSucursal={false}
+                    product={trueque.solicitud.offered_product}
+                />
+            </div>
         </div>
     )
 }
