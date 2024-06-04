6425abed3ee6ac5206839188796a18e50929550d
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-03 21:21:59 -0300

    Agrega columna state y elimina columna was_rejected

--- database/migrations/2024_05_26_210701_create_solicituds_table.php
+++ database/migrations/2024_05_26_210701_create_solicituds_table.php
@@ -16,7 +16,7 @@ return new class extends Migration {
             $table->foreignId('published_product_id')->constrained('products');
             $table->foreignId('offered_product_id')->constrained('products');
             $table->dateTime('meeting_date_time');
-            $table->boolean('was_rejected')->default(false);
+            $table->enum('state', ['normal', 'accepted', 'frozen', 'rejected']);
             $table->softDeletes();
             $table->timestamps();
         });
