93afa2e59b64d427ef9c131f23cfad0821354965
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-03 21:16:34 -0300

    Arregla altura

--- resources/js/Components/Divisor.jsx
+++ resources/js/Components/Divisor.jsx
@@ -1,4 +1,9 @@
-export default function Divisor({ className }) {
+export default function Divisor({ vertical = false, className }) {
+    if(vertical){
+        return (
+            <div className={`border-l border-gray-600 h-10 ${className}`}></div>
+        )
+    }
     return (
         <div className={`border-b border-gray-600 w-full ${className}`}></div>
     )
