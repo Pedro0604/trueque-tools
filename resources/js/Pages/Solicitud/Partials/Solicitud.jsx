bd5e7c5c436df507f373628649a75f94038fab57
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-03 21:08:17 -0300

    La solicitud ahora hace uso de Product

--- resources/js/Pages/Solicitud/Partials/Solicitud.jsx
+++ resources/js/Pages/Solicitud/Partials/Solicitud.jsx
@@ -2,6 +2,7 @@ import ImageNotSupportedIcon from '@mui/icons-material/ImageNotSupported';
 import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 import {router} from "@inertiajs/react";
 import DangerButton from "@/Components/Buttons/DangerButton.jsx";
+import Product from "@/Pages/Product/Partials/Product.jsx";
 
 export default function Solicitud({solicitud}) {
     const {offered_product, published_product} = solicitud;
@@ -23,41 +24,23 @@ export default function Solicitud({solicitud}) {
             className={`text-black dark:text-white border border-custom-cyan-800 dark:border-custom-cyan-400 rounded-md p-4 relative`}
         >
             <div>
-                <div className="flex items-center justify-between">
-                    <div className="font-semibold text-sm text-gray-600 dark:text-gray-500">
-                        {offered_product.user.name}
-                    </div>
-                    <div className="text-sm text-gray-600 dark:text-gray-500 ml-2">
-                        Fecha: {solicitud.meeting_date_time}
+                <div className="text-2xl text-center text-gray-800 dark:text-gray-300 ml-2">
+                    Fecha propuesta
+                    <div
+                        className="text-lg text-gray-800 dark:text-gray-300"
+                    >
+                        {solicitud.meeting_date_time}
                     </div>
                 </div>
                 <div>
-                    <div className="font-semibold text-lg text-gray-600 dark:text-gray-300">
-                        {offered_product.name}
-                    </div>
-                    <div
+                    <Product
+                        product={offered_product}
                         onClick={() => showProduct(offered_product.id)}
-                        className="cursor-pointer"
-                    >
-                        {offered_product.image_path ?
-                            <img
-                                src={offered_product.image_path}
-                                alt={offered_product.name}
-                                className={`object-cover w-full aspect-square rounded-md  border border-custom-beige-900 dark:border-custom-beige-500`}
-                            />
-                            :
-                            <div
-                                className="flex items-center justify-center w-full aspect-square rounded-md bg-gray-200 text-gray-600 dark:bg-gray-600 dark:text-gray-200">
-                                <p className="hidden sm:inline">IMAGEN NO ENCONTRADA</p>
-                                <span className="sm:hidden">
-                                <ImageNotSupportedIcon
-                                    sx={{fontSize: 32}}
-                                />
-                            </span>
-                            </div>
-                        }
-                    </div>
-                    <div className="flex flex-col gap-2 mt-4">
+                        className="border-none px-0"
+                        withCategory={false}
+                        withSucursal={false}
+                    />
+                    <div className="flex flex-col gap-2 mt-8">
                         <CyanButton
                             className="w-full justify-center"
                             onClick={() => acceptSolicitud(solicitud.id)}
