4fbedf02f98592025dbf8d8e94ad88206c9a7161
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-09 20:08:40 -0300

    Añade canBeAccepted y canBeRejected

--- app/Http/Resources/SolicitudResource.php
+++ app/Http/Resources/SolicitudResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Solicitud;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -24,6 +25,8 @@ class SolicitudResource extends JsonResource
             'state' => $this->state,
             'created_at' => (new Carbon($this->created_at))->format('d/m/Y H:i'),
             'canBeViewed' => Gate::allows('view', $this->resource),
+            'canBeAccepted' => Gate::allows('accept', [Solicitud::class, $this->publishedProduct]),
+            'canBeRejected' => Gate::allows('reject', [Solicitud::class, $this->publishedProduct]),
         ];
     }
 }
