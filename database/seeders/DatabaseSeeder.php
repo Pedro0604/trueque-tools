0f6317f540ddba46b6b0d9e2e0bdd8ace3bdc6ad
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-10 11:39:05 -0300

    Seeder para demo2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Admin;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -24,10 +25,7 @@ class DatabaseSeeder extends Seeder
 //            AdminSeeder::class,
 //            EmpleadoSeeder::class,
 //        ]);
-
-        $this->call([
-            AdminSeeder::class,
-        ]);
+        Admin::factory()->create();
 
         User::factory()->create([
             'name' => 'Test User',
