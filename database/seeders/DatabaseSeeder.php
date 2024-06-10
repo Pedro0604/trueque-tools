9710242b55a18957dd0511ab20800cb20c8d49c0
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-10 11:52:23 -0300

    Seeder para demo2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,16 +26,5 @@ class DatabaseSeeder extends Seeder
 //            EmpleadoSeeder::class,
 //        ]);
         Admin::factory()->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@gmail.com',
-            'password' => bcrypt('12341234'),
-        ]);
-        User::factory()->create([
-            'name' => 'Test User 2',
-            'email' => 'test2@gmail.com',
-            'password' => bcrypt('12341234'),
-        ]);
     }
 }
