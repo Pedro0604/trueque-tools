6f8abfa169e931f10fe6f9d62df914abf66059ce
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-14 23:45:30 -0300

    Agrega queryParams para filtrar por sucursal y/o categoría

--- resources/js/Pages/Product/Index.jsx
+++ resources/js/Pages/Product/Index.jsx
@@ -1,11 +1,30 @@
-import {Head, usePage} from "@inertiajs/react";
+import {Head, router, usePage} from "@inertiajs/react";
 import AuthenticatedOrNormalLayout from "@/Layouts/AuthenticatedOrNormalLayout.jsx";
 import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 import AddIcon from '@mui/icons-material/Add';
 import ProductsList from "@/Pages/Product/Partials/ProductsList.jsx";
+import SelectInput from "@/Components/Inputs/SelectInput.jsx";
+import SucursalOptions from "@/Pages/Sucursal/Partials/SucursalOptions.jsx";
+import CategoryOptions from "@/Components/CategoryOptions.jsx";
 
 export default function Index({products, productCreatedId = null}) {
-    const auth = usePage().props.auth;
+    const props = usePage().props;
+    const auth = props.auth
+    const queryParams = Object.assign({}, props.queryParams);
+
+    const searchFieldChanged = (field, value) => {
+        if (value) {
+            queryParams[field] = value;
+        } else {
+            delete queryParams[field];
+        }
+
+        router.get(route('product.index', queryParams));
+    }
+
+    const emptyListMessage = `No hay productos cargados en el sistema ${queryParams.sucursal || queryParams.category ? 'para el filtro seleccionado' : ''}`;
+
+
     return (
         <AuthenticatedOrNormalLayout
             header={
@@ -13,15 +32,39 @@ export default function Index({products, productCreatedId = null}) {
                     <h2 className="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                         Listado de Productos
                     </h2>
-                    {auth.user &&
-                        <CyanButton
-                            isLink
-                            href={route('product.create')}
+                    <div className="flex gap-8 justify-evenly">
+                        <SelectInput
+                            id="sucursal"
+                            name="sucursal"
+                            onChange={(e) => {
+                                searchFieldChanged('sucursal', e.target.value)
+                            }}
+                            defaultValue={queryParams.sucursal}
                         >
-                            <span className="hidden sm:block">Agregar nuevo producto</span>
-                            <span className="sm:hidden"><AddIcon/></span>
-                        </CyanButton>
-                    }
+                            <option value="">Sucursal</option>
+                            <SucursalOptions/>
+                        </SelectInput>
+                        <SelectInput
+                            id="category"
+                            name="category"
+                            onChange={(e) => {
+                                searchFieldChanged('category', e.target.value)
+                            }}
+                            defaultValue={queryParams.category}
+                        >
+                            <option value="">Categoría</option>
+                            <CategoryOptions/>
+                        </SelectInput>
+                        {auth.user &&
+                            <CyanButton
+                                isLink
+                                href={route('product.create')}
+                            >
+                                <span className="hidden sm:block">Agregar nuevo producto</span>
+                                <span className="sm:hidden"><AddIcon/></span>
+                            </CyanButton>
+                        }
+                    </div>
                 </div>
             }
         >
@@ -30,7 +73,7 @@ export default function Index({products, productCreatedId = null}) {
             <ProductsList
                 products={products}
                 productCreatedId={productCreatedId}
-                emptyListMessage="No hay productos cargados en el sistema"
+                emptyListMessage={emptyListMessage}
                 blurIfPaused
             />
         </AuthenticatedOrNormalLayout>
