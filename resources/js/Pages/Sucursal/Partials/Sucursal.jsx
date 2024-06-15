960a31fd56d73905c293097de50c73ddeda51a78
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-14 23:51:09 -0300

    Agrega botón para ver los productos de la sucursal

--- resources/js/Pages/Sucursal/Partials/Sucursal.jsx
+++ resources/js/Pages/Sucursal/Partials/Sucursal.jsx
@@ -1,11 +1,12 @@
 import BusinessIcon from '@mui/icons-material/Business';
+import CyanButton from "@/Components/Buttons/CyanButton.jsx";
 
 
 export default function Sucursal({
                                      sucursal
                                  }) {
     return (
-        <div className="border border-cyan-500 rounded-md p-4 lg:hover:bg-gray-200 lg:hover:dark:bg-custom-gray-700 lg:hover:shadow-2xl cursor-pointer">
+        <div className="border border-cyan-500 rounded-md p-4">
             <div className="flex justify-between mb-4">
                 <BusinessIcon></BusinessIcon>
                 <p className="text-sm sm:text-base text-gray-600 dark:text-custom-beige-600">
@@ -18,6 +19,13 @@ export default function Sucursal({
             <p className="text-sm sm:text-base lg:text-lg text-gray-600 dark:text-custom-beige-600">
                 Dirección: {sucursal.address}
             </p>
+            <CyanButton
+                href={route('product.index', {sucursal: sucursal.id})}
+                isLink
+                className="w-full justify-center mt-4"
+            >
+                Ver productos
+            </CyanButton>
         </div>
     )
 }
