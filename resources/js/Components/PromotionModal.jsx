96875f04c58ec14862f7ab27fd7c65ad9dc94741
Pedro0604 <pedrospadari0604@gmail.com>
2024-06-20 16:49:20 -0300

    Añade icono de tarjeta de credito

--- resources/js/Components/PromotionModal.jsx
+++ resources/js/Components/PromotionModal.jsx
@@ -1,12 +1,18 @@
 import Modal from "@mui/material/Modal";
-import {usePage} from "@inertiajs/react";
-import {initMercadoPago, Wallet} from '@mercadopago/sdk-react'
+import { usePage } from "@inertiajs/react";
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
 import PrimaryButton from "@/Components/Buttons/PrimaryButton.jsx";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
 
-initMercadoPago('APP_USR-66c048b3-9b12-4fb2-8961-7cea14c5a0a4');
+initMercadoPago("APP_USR-66c048b3-9b12-4fb2-8961-7cea14c5a0a4");
 
-export default function PromotionModal({open, handleClose, productId, preferenceId}) {
-    const {csrf_token} = usePage().props;
+export default function PromotionModal({
+    open,
+    handleClose,
+    productId,
+    preferenceId,
+}) {
+    const { csrf_token } = usePage().props;
 
     return (
         <Modal
@@ -16,11 +22,9 @@ export default function PromotionModal({open, handleClose, productId, preference
             aria-describedby="modal-modal-description"
             className="flex items-center justify-center"
         >
-            <div
-                className="flex flex-col gap-2 items-center bg-gray-600 pt-10 pb-2 px-10 rounded-lg"
-            >
+            <div className="flex flex-col gap-2 items-center bg-gray-600 pt-10 pb-2 px-10 rounded-lg">
                 <form
-                    action={route('promotion.stripe.promote', productId)}
+                    action={route("promotion.stripe.promote", productId)}
                     method="POST"
                     className="w-full"
                 >
@@ -31,16 +35,16 @@ export default function PromotionModal({open, handleClose, productId, preference
                         autoComplete="off"
                     />
 
-                    <PrimaryButton
-                        className="w-full justify-center h-12 !text-sm"
-                    >
-                        Pagar con tarjeta
+                    <PrimaryButton className="w-full justify-center h-12 !text-sm">
+                        Pagar con tarjeta <CreditCardIcon className="ml-2" />
                     </PrimaryButton>
                 </form>
-                <Wallet initialization={{preferenceId: preferenceId}}
-                        customization={{texts: {valueProp: 'smart_option'}}}/>
+                <Wallet
+                    initialization={{ preferenceId: preferenceId }}
+                    customization={{ texts: { valueProp: "smart_option" } }}
+                />
                 <div id="wallet_container"></div>
             </div>
         </Modal>
-    )
+    );
 }
