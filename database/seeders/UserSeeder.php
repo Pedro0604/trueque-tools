448b7df78ae5252f98a8a2661065ff3e115660aa
frand <fran.dellarupe03@gmail.com>
2024-07-08 13:40:12 -0300

    user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,25 +13,25 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test',
+            'name' => 'Test User',
             'surname' => 'User',
             'email' => 'test@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
         User::factory()->create([
-            'name' => 'Test',
+            'name' => 'Test User 2',
             'surname' => 'User 2',
             'email' => 'test2@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
         User::factory()->create([
-            'name' => 'Test',
+            'name' => 'Test User 3',
             'surname' => 'User 3',
             'email' => 'test3@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
         User::factory()->create([
-            'name' => 'Test',
+            'name' => 'Test User 4',
             'surname' => 'User 4',
             'email' => 'test4@gmail.com',
             'password' => bcrypt('12341234'),
