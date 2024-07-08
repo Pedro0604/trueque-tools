e4180a0994ce64e238cb6952f58948549cd46c5f
Pedro0604 <pedrospadari0604@gmail.com>
2024-07-08 13:00:00 -0300

    Apellidos de los usuarios para la demo 3

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,22 +13,26 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Test',
+            'surname' => 'User',
             'email' => 'test@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
         User::factory()->create([
-            'name' => 'Test User 2',
+            'name' => 'Test',
+            'surname' => 'User 2',
             'email' => 'test2@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
         User::factory()->create([
-            'name' => 'Test User 3',
+            'name' => 'Test',
+            'surname' => 'User 3',
             'email' => 'test3@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
         User::factory()->create([
-            'name' => 'Test User 4',
+            'name' => 'Test',
+            'surname' => 'User 4',
             'email' => 'test4@gmail.com',
             'password' => bcrypt('12341234'),
         ]);
